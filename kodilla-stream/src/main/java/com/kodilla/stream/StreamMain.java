5c99f850304366061455db4b8a0475b5ddf9233e
Desdem0n <desmilke@gmail.com>
2018-12-27 16:35:13 +0100

    task 7.2 done

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,27 +3,49 @@ package com.kodilla.stream;
 //import com.kodilla.stream.lambda.ExecuteSaySomething;
 //import com.kodilla.stream.lambda.Processor;
 //import com.kodilla.stream.lambda.SaySomething;
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExpressionExecutor;
+//import com.kodilla.stream.beautifier.PoemBeautifier;
+//import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.people.Person;
 
 public class StreamMain {
     public static void main(String[] args) {
+        Person.getList().stream()
+                .map(s -> s.toUpperCase())
+                .forEach(System.out::println);
+
+        System.out.println(" ");
+
+        Person.getList().stream()
+                .map(s -> s.toLowerCase())
+                .forEach(System.out::println);
+
+        System.out.println(" ");
+
+        Person.getList().stream()
+                .map(s -> s.charAt(3))
+                .forEach(System.out::println);
+
+        System.out.println(" ");
+
+        Person.getList().stream()
+                .map(s -> s.charAt(5))
+                .forEach(System.out::println);
+
 //        System.out.println("Welcome to module 7 - Stream");
 //        Processor processor = new Processor();
 //        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
-////        SaySomething saySomething = new SaySomething();
-////        saySomething.say();
+//        SaySomething saySomething = new SaySomething();
+//        saySomething.say();
 //        processor.execute(executeSaySomething);
+//
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//
+//        poemBeautifier.beautify("HWDP", "SetTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated + decoration));
+//        poemBeautifier.beautify("","SetTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated.toUpperCase()));
+//        poemBeautifier.beautify("*-*", "SetTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated.toLowerCase() + decoration));
+//        poemBeautifier.beautify("<>", "SetTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated.replace('T', 't') + decoration));
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        poemBeautifier.beautify("HWDP", "SetTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated + decoration));
-        poemBeautifier.beautify("","SetTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated.toUpperCase()));
-        poemBeautifier.beautify("*-*", "SetTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated.toLowerCase() + decoration));
-        poemBeautifier.beautify("<>", "SetTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated.replace('T', 't') + decoration));
-
-        System.out.println("Using Stream to generate even numbers from 0 to 20");
-        NumbersGenerator.generateEven(20);
+        //System.out.println("Using Stream to generate even numbers from 0 to 20");
+        //NumbersGenerator.generateEven(200);
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
@@ -6,8 +6,9 @@ public final class NumbersGenerator {
     public static void generateEven(int max) {
         Stream.iterate(1, n -> n + 1)
                 .limit(max)
-                .filter(n -> n % 2 == 0)
+                .filter(n -> n % 2 != 0)
                 .forEach(System.out::println);
+
     }
 
 
